eef34573771ad9293474c4334ba576fa4a9df58e
MotoakiFukuda <motoaki.fukuda@bravi.co.jp>
2017-07-21 08:08:22 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Item;
+
 class ItemsController extends Controller
 {
     /**
@@ -74,7 +76,13 @@ class ItemsController extends Controller
      */
     public function show($id)
     {
-        //
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
     }
 
     /**
